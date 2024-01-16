a9cd4ee1052ba99ad13edad1de74f02829e1b6b1
Eden Quiroga <edenqrg@gmail.com>
2024-01-16 15:58:16 -0600

    Ejercicio 1 LDD terminado

--- Ejercicio1_LDD.sql
+++ Ejercicio1_LDD.sql
@@ -20,3 +20,22 @@ CREATE TABLE tblcategoria(
 )
 go
 --Crear la tabla productos
+CREATE TABLE tblProducto(
+productoId int not null,
+nombreP varchar(50) not null,
+precio decimal(10,2) not null,
+existencia int not null,
+categoriaId int not null,
+constraint pk_tblProducto
+primary key(productoId),
+constraint unique_nombreP 
+unique(nombreP),
+constraint chk_precio
+check(precio>0.0 and precio<=10000),
+constraint chk_existencia
+check(existencia>=0),
+constraint fk_tblProducto_tblcategoria
+foreign key(categoriaId)
+references tblcategoria(categoriaId)
+
+)
