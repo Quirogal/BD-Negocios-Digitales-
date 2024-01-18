149f9f9b3af79305d0f9d2226ea76a00e3634521
Eden Quiroga <edenqrg@gmail.com>
2024-01-18 16:38:45 -0600

    Commit con Ejercicio 2 Terminado

--- Practica2.sql
+++ Practica2.sql
@@ -64,15 +64,85 @@ check(Salario>=400 and Salario<=50000)
   CREATE TABLE Venta(
 IdVenta int not null,
 Fecha date not null,
+IdCliente int not null,
+IdEmpleado int not null,
 constraint fk_tblCliente_tblVenta
 foreign key(IdCliente)
-references tblCliente(IdCliente)
+references tblCliente(IdCliente),
 constraint fk_tblEmpleado_tblVenta
 foreign key(IdEmpleado)
 references tblEmpleado(IdEmpleado)
+ )
+ go
+ CREATE TABLE Producto(
+IdProducto int not null,
+Descripcion varchar(100) not null,
+Existencia varchar(100) not null,
+PrecioUnitario int not null,
+ constraint unique_Descripcion
+ unique(Descripcion)
+ )
+ go 
+
+ CREATE TABLE DetalleVenta(
+IdVenta int not null,
+IdProducto int  not null,
+Cantidad int not null,
+Precio int not null,
+constraint pk_DetalleVenta
+Primary Key(IdVenta,IdProducto),
+constraint fk_DetalleVenta_Venta
+foreign key (IdVenta)
+references Venta(IdVenta)
 
 
-
+ 
 
  )
  go
+
+ drop table Venta
+ drop table DetalleVenta
+ drop table Venta
+ drop table Producto
+
+ CREATE TABLE tblVenta (
+    IdVenta int not null,
+    fecha DATE,
+    IdCliente INT,
+    IdEmpleado INT,
+	constraint pk_Venta
+	primary key(IdVenta),
+    FOREIGN KEY (IdCliente) REFERENCES tblCliente(IdCliente),
+    FOREIGN KEY (IdEmpleado) REFERENCES tblEmpleado(IdEmpleado)
+)
+go
+
+CREATE TABLE tblDetalleVenta (
+IdVenta int not null,
+IdProducto int not null,
+Cantidad int not null,
+Precio decimal (10,2) not null,
+constraint pk_DetallesVenta
+primary key (IdVenta, IdProducto),
+foreign key (IdVenta) references tblVenta(IdVenta),
+)
+go
+
+CREATE TABLE tbl_Producto (
+    IdProducto INT NOT NULL,
+    Descripcion VARCHAR(200) NOT NULL,
+    Existencia INT NOT NULL,
+    Precio_Unitario DECIMAL(10,2) NOT NULL,
+    IdVenta INT NOT NULL,
+    IdEmpleado INT NOT NULL,
+    CONSTRAINT pk_Producto PRIMARY KEY (IdProducto),
+    CONSTRAINT fk_Venta FOREIGN KEY (IdVenta) REFERENCES tblVenta(IdVenta),
+    
+)
+go
+drop table tbl_Producto
+drop table tblEmpleado 
+
+drop table tblDetalleVenta
+drop table tblDetalleVenta
